cbff829e6d592e9e4c82383bc04376a0fd42b052
noah-boulton <noah.boulton@gmail.com>
2018-03-04 20:05:42 -0800

    Changed find food threshold

--- helpers/foodHelper.js
+++ helpers/foodHelper.js
@@ -3,6 +3,11 @@ function needFood(data){
   var wallHeight = data.height
   var wallWidth = data.width
   var dimSum = wallWidth + wallHeight
+  var numFood = data.food.data.length
+  var threshold = 50 - (10*numFood)
+  if(threshold > 0){
+  	dimSum += threshold
+  }
 
   if (snakeHealth <= dimSum) {
     return true;
