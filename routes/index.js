d4ca9c6b774f4380c2742304a585067f17cecd14
esrah <esrahb@upanup.com>
2018-03-10 09:14:26 -0800

    another test fix

--- routes/index.js
+++ routes/index.js
@@ -103,7 +103,7 @@ router.post('/move', function (req, res) {
   }
 
   if(snakes.length == 2 && snakes[1-index].body.data.length < jsonHelper.getBody(req)) {
-    var enemyName = req.snakes.data.body.data.name
+    var enemyName = snakes[1-index].body.data.name
     var tauntBoi = 'rip, '+enemyName
   } else {
     var tauntBoi = taunts[taunt];
