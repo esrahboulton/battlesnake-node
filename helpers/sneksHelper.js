d418b33f98502b0cf645fd09843cfdae313f495c
Noah Boulton <noah.boulton@gmail.com>
2019-02-28 22:35:02 -0800

    Adding our tail as a legal move

--- helpers/sneksHelper.js
+++ helpers/sneksHelper.js
@@ -1,8 +1,14 @@
+var jsonHelper = require('../helpers/jsonHelper')
+
 function avoidSneks(data, head, moveOptions) {
   var snakes = data.board.snakes
   for (i = 0; i < snakes.length; i++) {
     var snek = snakes[i].body;
-    for (j = 0; j < snek.length; j++) {
+    var len = snek.length
+    if(snakes[i].id == jsonHelper.getID(data)){
+      len = len-1
+    }
+    for (j = 0; j < len; j++) {
       if (head.x - 1 == snek[j].x && head.y == snek[j].y) {
         //cant go left
         moveOptions[0] = false
