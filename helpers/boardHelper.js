f6a622a0757c9799c5de633d84506390f7573e1b
Wesley Yu <wes@metalab.co>
2019-03-02 15:47:11 -0800

    implement add score

--- helpers/boardHelper.js
+++ helpers/boardHelper.js
@@ -14,6 +14,7 @@ function setupBoard(req, dim, id){
 
   addSnakes(gameBoard, req.board.snakes)
   addFood(gameBoard, req.board.food);
+  addScore(gameBoard)
 
   return gameBoard
 }
@@ -28,6 +29,33 @@ function addSnakes(board, snakes) {
   snakes.forEach((snake) => {
     snake.body.forEach((bodyDims) => {
       board[bodyDims.x][bodyDims.y].contents = SNAKE_BODY
+    })
+  })
+}
+
+function addScore(board) {
+  board.forEach((row, xCord) => {
+    row.forEach((square, yCord) => {
+      let score = 0;
+      const top = board[xCord - 1] && board[xCord - 1][yCord]
+      const right = board[xCord] && board[xCord][yCord + 1]
+      const bottom = board[xCord + 1] && board[xCord + 1][yCord]
+      const left = board[xCord] && board[xCord][yCord - 1]
+
+      if (top && top.contents === EMPTY) {
+        score += 1
+      }
+      if (right && right.contents === EMPTY) {
+        score += 1
+      }
+      if (bottom &&  bottom.contents === EMPTY) {
+        score += 1
+      }
+      if (left && left.contents === EMPTY) {
+        score += 1
+      }
+
+      square.score = score;
     })
   })
 }
