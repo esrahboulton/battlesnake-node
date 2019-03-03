19d273f19bd558d7b47e3d631c56e71a12d451ad
esrah <esrah.boulton@gmail.com>
2019-03-02 17:01:53 -0800

    added needsFood check to index

--- routes/index.js
+++ routes/index.js
@@ -68,7 +68,7 @@ router.post('/move', function(req, res) {
 
   var killMove = killHelper.kill(req, snakeHead)
 
-  if (snakes.length == 2 && snakes[1 - index].body.length < jsonHelper.getBody(req)) {
+  if (snakes.length == 2 && snakes[1 - index].body.length < jsonHelper.getBody(req) && !needsFood) {
     // 1v1 time. We are king snek, actively kill the other snek
     var enemyName = snakes[1 - index].name
     var tauntBoi = 'rip, ' + enemyName
